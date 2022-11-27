a3080382e089c8ac0f1f94f1d0960333bb04dc02
velengel <koutyan_dora_ouchi@yahoo.co.jp>
2022-11-27 20:48:33 +0900

    wip: add: doing, done, fix: filter

--- src/components/ThreeLists.vue
+++ src/components/ThreeLists.vue
@@ -1,7 +1,25 @@
-<script setup lang="ts">
-import TodoListItem from "./TodoList.vue";
+<script>
+import TodoList from "./TodoList.vue";
+let id = 0
+export default {
+  components: {
+    TodoList
+  },
+  data() {
+    return {
+      todos: [
+        { id: id++, text: "Learn HTML", done: false, statusNum: 0 },
+        { id: id++, text: "Learn JavaScript", done: false, statusNum: 1 },
+        { id: id++, text: "Learn Vue", done: false, statusNum: 2 },
+      ]
+    }
+
+  }
+}
 </script>
 
 <template>
-  <TodoListItem />
+  <TodoList :todos="todos"
+    @syncTodos="(changedTodos) => (todos = changedTodos)"
+    />
 </template>
